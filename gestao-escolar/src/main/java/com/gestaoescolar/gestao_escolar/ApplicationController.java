1f7958862302f253fc688ce305afa877750f7880
Matheus Mello <mat.mello93@gmail.com>
2017-04-23 20:15:27 -0300

    starting project

---
+++ gestao-escolar/src/main/java/com/gestaoescolar/gestao_escolar/ApplicationController.java
@@ -0,0 +1,15 @@
+package com.gestaoescolar.gestao_escolar;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class ApplicationController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Gestao Escolar App";
+    }
+
+}
